782f9165eed66f346434bbde2376f72a50ae3297
Nadua <vladickschaf@gmail.com>
2025-07-27 18:06:14 +0300

    about me

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,10 +1,20 @@
 import {base_url, period_month} from "../utils/constants.ts";
 import {useEffect, useState} from "react";
 
+interface infoProps {
+    name: string,
+    gender: string,
+    birth_year: string,
+    mass: number,
+    hair_color: string,
+    skin_color: string,
+    eye_color: string,
+}
+
 const AboutMe = () => {
-    const [hero, setHero] = useState();
+    const [hero, setHero] = useState<infoProps>();
     useEffect(() => {
-        const hero = JSON.parse(localStorage.getItem("hero"));
+        const hero = JSON.parse(localStorage.getItem("hero")!);
         if (hero && ((Date.now() - hero.timestamp) < period_month)) {
             setHero(hero.payload);
         } else {
@@ -35,7 +45,7 @@ const AboutMe = () => {
             {(!!hero) &&
                 <div className={'text-[2em] text-justify tracking-widest leading-14 ml-8'}>
                     {Object.keys(hero).map(key => <p key={key}>
-                        <span className={'text-3xl capitalize'}>{key.replace('_', ' ')}</span>: {hero[key]}
+                        <span className={'text-3xl capitalize'}>{key.replace('_', ' ')}</span>: {hero[key as keyof infoProps]}
                     </p>)}
                 </div>
             }
